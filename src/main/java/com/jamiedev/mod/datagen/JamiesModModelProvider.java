2f7af122a51fd643db54cd080c3928a7bc91c262
JRepo42 <JRepo42@users.noreply.github.com>
2024-10-23 20:58:46 +0100

    Caster datagen working

--- src/main/java/com/jamiedev/mod/datagen/JamiesModModelProvider.java
+++ src/main/java/com/jamiedev/mod/datagen/JamiesModModelProvider.java
@@ -1,6 +1,7 @@
 package com.jamiedev.mod.datagen;
 import com.jamiedev.mod.init.JamiesModBlocks;
 import com.jamiedev.mod.init.JamiesModItems;
+import com.jamiedev.mod.util.DatagenUtils;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
@@ -22,7 +23,7 @@ public class JamiesModModelProvider  extends FabricModelProvider {
 
         //malachite.family(BlockFamilies.register(JamiesModBlocks.MALACHITE).slab(JamiesModBlocks.MALACHITE_SLAB).stairs(JamiesModBlocks.MALACHITE_STAIRS).wall(JamiesModBlocks.MALACHITE_WALL).build());
         //tiles.family(BlockFamilies.register(JamiesModBlocks.MALACHITE_TILE).slab(JamiesModBlocks.MALACHITE_TILE_SLAB).stairs(JamiesModBlocks.MALACHITE_TILE_STAIRS).wall(JamiesModBlocks.MALACHITE_TILE_WALL).build());
-        blockStateModelGenerator.registerDispenserLikeOrientable(JamiesModBlocks.CASTER);
+        DatagenUtils.registerCasterModel(JamiesModBlocks.CASTER, blockStateModelGenerator);
     }
 
     @Override
