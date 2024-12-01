4cbebd1dc2a47503e51d36bdfa7beced525d3b54
JRepo42 <JRepo42@users.noreply.github.com>
2024-12-01 22:11:08 +0000

    Refined livingentity mixin

--- src/main/java/com/jamiedev/mod/mixin/LivingEntityMixin.java
+++ src/main/java/com/jamiedev/mod/mixin/LivingEntityMixin.java
@@ -2,7 +2,12 @@ package com.jamiedev.mod.mixin;
 
 import com.jamiedev.mod.common.items.VerdigrisBladeItem;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,6 +20,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class LivingEntityMixin {
     @Shadow public abstract ItemStack getActiveItem();
 
+    @Shadow protected abstract void playHurtSound(DamageSource damageSource);
+
+    @Shadow public abstract @Nullable DamageSource getRecentDamageSource();
+
+    @Shadow public abstract void playSound(@Nullable SoundEvent sound);
+
     // Credits to https://github.com/Quplet/NoShieldDelay (MIT Licensed)
     @ModifyConstant(method = "isBlocking", constant = @Constant(intValue = 5))
     private int jamies_mod$isBlocking(int constant) {
@@ -23,6 +34,9 @@ public abstract class LivingEntityMixin {
 
     @Inject(method = "handleStatus", at = @At("HEAD"), cancellable = true)
     public void jamies_mod$handleStatus(byte status, CallbackInfo ci) {
-        if (status == 29) ci.cancel();
+        if ((LivingEntity) (Object) this instanceof PlayerEntity player && player.getActiveItem().getItem() instanceof VerdigrisBladeItem) {
+            if (status == 29) ci.cancel();
+            this.playSound(SoundEvents.ENTITY_GENERIC_HURT);
+        }
     }
 }
