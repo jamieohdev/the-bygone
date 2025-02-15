9e6230a0e62d4d41e3091eef1da5f5bc17917af8
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-02-15 20:12:12 +0000

    Ancient signs can now be used as fuel

--- neoforge/src/main/java/com/jamiedev/bygone/datagen/BygoneItemTagProvider.java
+++ neoforge/src/main/java/com/jamiedev/bygone/datagen/BygoneItemTagProvider.java
@@ -1,9 +1,12 @@
 package com.jamiedev.bygone.datagen;
 
 import com.jamiedev.bygone.Bygone;
+import com.jamiedev.bygone.init.JamiesModBlocks;
+import com.jamiedev.bygone.init.JamiesModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -18,7 +21,13 @@ public class BygoneItemTagProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider arg) {
+        signs();
+   }
 
-
+   private void signs() {
+        this.tag(ItemTags.SIGNS)
+                .add(JamiesModBlocks.ANCIENT_SIGN.asItem());
+       this.tag(ItemTags.HANGING_SIGNS)
+               .add(JamiesModItems.ANCIENT_HANGING_SIGN);
    }
 }
