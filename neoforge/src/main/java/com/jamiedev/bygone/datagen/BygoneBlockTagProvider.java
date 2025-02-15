a19af53bb2722fb73e7884261819691b5eb3848a
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-02-15 21:16:20 +0000

    Belladonna and Coleus tag fixes

--- neoforge/src/main/java/com/jamiedev/bygone/datagen/BygoneBlockTagProvider.java
+++ neoforge/src/main/java/com/jamiedev/bygone/datagen/BygoneBlockTagProvider.java
@@ -1,8 +1,10 @@
 package com.jamiedev.bygone.datagen;
 
 import com.jamiedev.bygone.Bygone;
+import com.jamiedev.bygone.init.JamiesModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -18,11 +20,39 @@ public class BygoneBlockTagProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider arg) {
+        swordEfficient();
+        flowers();
+        insideStepSoundBlocks();
+        mineableWithHoe();
        /* tag(BlockTags.MINEABLE_WITH_AXE)
                 .add(JamiesModBlocks.CLAYSTONE_BRICKS)
                 .add(JamiesModBlocks.CLAYSTONE_BRICKS_STAIRS)
                 .add(JamiesModBlocks.CLAYSTONE_BRICKS_WALL)
                 .add(JamiesModBlocks.CLAYSTONE_BRICKS_SLAB);*///todo
         
+    }
+
+    private void swordEfficient() {
+        this.tag(BlockTags.SWORD_EFFICIENT)
+                .add(JamiesModBlocks.BELLADONNA)
+                .add(JamiesModBlocks.COLEUS);
+    }
+
+    private void flowers() {
+        this.tag(BlockTags.FLOWERS)
+                .add(JamiesModBlocks.BELLADONNA)
+                .add(JamiesModBlocks.COLEUS);
+    }
+
+    private void insideStepSoundBlocks() {
+        this.tag(BlockTags.INSIDE_STEP_SOUND_BLOCKS)
+                .add(JamiesModBlocks.BELLADONNA)
+                .add(JamiesModBlocks.COLEUS);
+    }
+
+    private void mineableWithHoe() {
+        this.tag(BlockTags.MINEABLE_WITH_HOE)
+                .add(JamiesModBlocks.BELLADONNA)
+                .add(JamiesModBlocks.COLEUS);
     }
 }
