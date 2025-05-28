3e32b869725bd8b900bd5e21903db2ea35cad7e2
Exavior <pro.xavieragonz@gmail.com>
2025-05-28 01:54:52 -0500

    Started on the Hydro

---
+++ common/src/main/java/com/jamiedev/bygone/common/entity/HydropusEntity.java
@@ -0,0 +1,117 @@
+package com.jamiedev.bygone.common.entity;
+
+import com.google.common.collect.ImmutableList;
+import com.jamiedev.bygone.common.entity.ai.GlareBrain;
+import com.jamiedev.bygone.common.entity.ai.HydropusBrain;
+import com.mojang.serialization.Dynamic;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier.Builder;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.sensing.Sensor;
+import net.minecraft.world.entity.ai.sensing.SensorType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class HydropusEntity extends Animal implements NeutralMob {
+    protected static final ImmutableList<SensorType<? extends Sensor<? super HydropusEntity>>> SENSOR_TYPES = ImmutableList.of(
+            SensorType.NEAREST_LIVING_ENTITIES,
+            SensorType.NEAREST_PLAYERS,
+            SensorType.HURT_BY
+    );
+    protected static final ImmutableList<MemoryModuleType<?>> MEMORY_TYPES = ImmutableList.of(
+            MemoryModuleType.PATH,
+            MemoryModuleType.LOOK_TARGET,
+            MemoryModuleType.NEAREST_LIVING_ENTITIES,
+            MemoryModuleType.NEAREST_VISIBLE_LIVING_ENTITIES,
+            MemoryModuleType.NEAREST_VISIBLE_PLAYER,
+            MemoryModuleType.HURT_BY,
+            MemoryModuleType.HURT_BY_ENTITY,
+            MemoryModuleType.WALK_TARGET,
+            MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE,
+            MemoryModuleType.ATTACK_TARGET,
+            MemoryModuleType.ATTACK_COOLING_DOWN,
+            MemoryModuleType.INTERACTION_TARGET,
+            MemoryModuleType.ANGRY_AT,
+            MemoryModuleType.UNIVERSAL_ANGER,
+            MemoryModuleType.NEAREST_ATTACKABLE
+    );
+
+    protected HydropusEntity(EntityType<? extends HydropusEntity> entityType, Level level) {
+        super(entityType, level); //BGEntityTypes.HYDROPUS.get()
+        // Will fix up constructor when it's ready
+    }
+
+    public static Builder createHydropusAttributes() {
+        return Animal.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 40.0)
+                .add(Attributes.ATTACK_DAMAGE, 5.0)
+                .add(Attributes.MOVEMENT_SPEED, 0.1);
+    }
+
+    @Override
+    protected Brain.Provider<HydropusEntity> brainProvider() {
+        return Brain.provider(MEMORY_TYPES, SENSOR_TYPES);
+    }
+    @Override
+    protected Brain<?> makeBrain(Dynamic<?> dynamic) {
+        return HydropusBrain.create(dynamic, MEMORY_TYPES, SENSOR_TYPES, this);
+    }
+    @Override
+    @SuppressWarnings("all")
+    public Brain<HydropusEntity> getBrain() {
+        return (Brain<HydropusEntity>) super.getBrain();
+    }
+
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType spawnType, @Nullable SpawnGroupData spawnGroupData) {
+        if (spawnType == MobSpawnType.STRUCTURE){
+            // Need to ask if they are immediately hostile
+        }
+
+        return super.finalizeSpawn(level, difficulty, spawnType, spawnGroupData);
+    }
+
+    @Override
+    public boolean isFood(ItemStack itemStack) {
+        return false;
+    }
+
+    @Override
+    public @Nullable AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
+        return null;
+    }
+
+    @Override
+    public int getRemainingPersistentAngerTime() {
+        return 0;
+    }
+
+    @Override
+    public void setRemainingPersistentAngerTime(int i) {
+
+    }
+
+    @Override
+    public @Nullable UUID getPersistentAngerTarget() {
+        return null;
+    }
+
+    @Override
+    public void setPersistentAngerTarget(@Nullable UUID uuid) {
+
+    }
+
+    @Override
+    public void startPersistentAngerTimer() {
+
+    }
+}
