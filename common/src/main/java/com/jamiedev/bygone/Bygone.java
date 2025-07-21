a2ba60a405657aa8c9e395575b2b697a64073df4
jamieohdev <118941965+jamieohdev@users.noreply.github.com>
2025-07-21 12:55:13 -0400

    Wisp

--- common/src/main/java/com/jamiedev/bygone/Bygone.java
+++ common/src/main/java/com/jamiedev/bygone/Bygone.java
@@ -89,6 +89,7 @@ public class Bygone {
         consumer.accept(BGEntityTypes.WHISKBILL.get(), WhiskbillEntity.createAttributes().build());
         consumer.accept(BGEntityTypes.NECTAUR.get(), NectaurEntity.createAttributes().build());
         consumer.accept(BGEntityTypes.LITHY.get(), LithyEntity.createAttributes().build());
+        consumer.accept(BGEntityTypes.WISP.get(), WispEntity.createAttributes().build());
         consumer.accept(BGEntityTypes.WRAITH.get(), WraithEntity.createAttributes().build());
     }
 
@@ -107,6 +108,7 @@ public class Bygone {
         consumer.accept((EntityType<T>) BGEntityTypes.NECTAUR.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING, BigBeakEntity::canSpawn);
         consumer.accept((EntityType<T>) BGEntityTypes.WHISKBILL.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING, BigBeakEntity::canSpawn);
         consumer.accept((EntityType<T>) BGEntityTypes.LITHY.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING, LithyEntity::canSpawn);
+        consumer.accept((EntityType<T>) BGEntityTypes.WISP.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING, LithyEntity::canSpawn);
         consumer.accept((EntityType<T>) BGEntityTypes.WRAITH.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING, LithyEntity::canSpawn);
     }
 
