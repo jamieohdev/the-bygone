a1c766236f0fdf4710add3c18fbaec4b6aa7dd24
Klinbee <imquidvio@gmail.com>
2026-02-04 12:51:08 -0600

    TESTING: commented out code in client that wasn't working to allow compilation

--- common/src/main/java/com/jamiedev/bygone/client/BygoneClient.java
+++ common/src/main/java/com/jamiedev/bygone/client/BygoneClient.java
@@ -194,8 +194,8 @@ public class BygoneClient {
         consumer.accept(BGBlocks.PAINTED_DOGU.get(), RenderType.cutout());
         consumer.accept(BGBlocks.SHELLSTONE_DOGU.get(), RenderType.cutout());
 
-        ClientHelper.registerRenderType(ModFluids.LUMISENE_BLOCK.get(), RenderType.cutout());
-        ClientHelper.registerFluidRenderType(ModFluids.LUMISENE_FLUID.get(), RenderType.translucent());
+//        ClientHelper.registerRenderType(ModFluids.LUMISENE_BLOCK.get(), RenderType.cutout());
+//        ClientHelper.registerFluidRenderType(ModFluids.LUMISENE_FLUID.get(), RenderType.translucent());
     }
 
     public static void createEntityRenderers() {
