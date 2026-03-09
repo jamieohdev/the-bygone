43ee6db95db4891b561e3b65ba19eba816f5e203
Jaai <arcticquests.dev@gmail.com>
2026-03-09 19:17:43 +0530

    Adds the ores to resources files and adds DuplicatesStrategy

--- neoforge/src/main/java/com/jamiedev/bygone/core/datagen/BygoneBlockLootSubProvider.java
+++ neoforge/src/main/java/com/jamiedev/bygone/core/datagen/BygoneBlockLootSubProvider.java
@@ -1,22 +1,16 @@
 package com.jamiedev.bygone.core.datagen;
 
 import com.jamiedev.bygone.Bygone;
-import com.jamiedev.bygone.core.registry.BGBlocks;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Supplier;
 
 public class BygoneBlockLootSubProvider extends BlockLootSubProvider {
 
