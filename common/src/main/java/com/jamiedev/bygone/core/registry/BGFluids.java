a3449f63584692a2f298928749be455ccccf94a6
jamieohdev <118941965+jamieohdev@users.noreply.github.com>
2026-08-30 08:15:22 -0400

    Fixed Fluid crash

--- common/src/main/java/com/jamiedev/bygone/core/registry/BGFluids.java
+++ common/src/main/java/com/jamiedev/bygone/core/registry/BGFluids.java
@@ -1,7 +1,6 @@
 package com.jamiedev.bygone.core.registry;
 
 import com.jamiedev.bygone.Bygone;
-import com.jamiedev.bygone.common.block.fluids.BronzeFluid;
 import com.jamiedev.bygone.common.block.fluids.LithoFluid;
 import com.kekecreations.jinxedlib.core.util.JinxedRegistryHelper;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -11,8 +10,6 @@ import java.util.function.Supplier;
 
 public class BGFluids {
 
-    public static Supplier<Fluid> BRONZE_STILL = registerFluid("bronze_still", BronzeFluid.Source::new);
-    public static Supplier<Fluid> BRONZE_FLOWING = registerFluid("bronze_flowing", BronzeFluid.Flowing::new);
     public static Supplier<Fluid> LITHO_STILL = registerFluid("litho", LithoFluid.Source::new);
     public static Supplier<Fluid> LITHO_FLOWING = registerFluid("flowing_litho", LithoFluid.Flowing::new);
 
