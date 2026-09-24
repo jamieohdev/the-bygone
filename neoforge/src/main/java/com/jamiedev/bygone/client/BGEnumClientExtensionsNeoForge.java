f2340c7db74732ce4a4e2082f8b95999d5309099
OfficialTurtleBoi <cmartz2000@gmail.com>
2026-09-24 15:48:14 -0500

    Fixed enum extension start error

--- neoforge/src/main/java/com/jamiedev/bygone/client/BGEnumClientExtensionsNeoForge.java
+++ neoforge/src/main/java/com/jamiedev/bygone/client/BGEnumClientExtensionsNeoForge.java
@@ -1,24 +1,23 @@
 package com.jamiedev.bygone.client;
 
-import com.jamiedev.bygone.Bygone;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.common.asm.enumextension.EnumProxy;
 
-@Mod(value = Bygone.MOD_ID, dist = Dist.CLIENT)
-public class BGEnumClientExtensionsNeoForge
-{
+public class BGEnumClientExtensionsNeoForge {
     public static final EnumProxy<Gui.HeartType> BYGONE_HEART_HAUNTED = new EnumProxy<>(
             Gui.HeartType.class,
-            Bygone.id( "hud/heart/haunted_full"),
-            Bygone.id( "hud/heart/haunted_full_blinking"),
-            Bygone.id( "hud/heart/haunted_half"),
-            Bygone.id( "hud/heart/haunted_half_blinking"),
-            Bygone.id( "hud/heart/haunted_hardcore_full"),
-            Bygone.id( "hud/heart/haunted_hardcore_full_blinking"),
-            Bygone.id( "hud/heart/haunted_hardcore_half"),
-            Bygone.id( "hud/heart/haunted_hardcore_half_blinking")
+            id("hud/heart/haunted_full"),
+            id("hud/heart/haunted_full_blinking"),
+            id("hud/heart/haunted_half"),
+            id("hud/heart/haunted_half_blinking"),
+            id("hud/heart/haunted_hardcore_full"),
+            id("hud/heart/haunted_hardcore_full_blinking"),
+            id("hud/heart/haunted_hardcore_half"),
+            id("hud/heart/haunted_hardcore_half_blinking")
     );
+
+    private static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath("bygone", path);
+    }
 }
